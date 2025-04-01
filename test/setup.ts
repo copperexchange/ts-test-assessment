ea400fee0f163fc40444ed65f32675f69fd163ee
Nikita Andrejevs <nikita.andrejevs@copper.co>
2025-04-01 14:59:12 +0300

    Get bin path for flyway

--- test/setup.ts
+++ test/setup.ts
@@ -2,10 +2,18 @@ import "reflect-metadata";
 import child_process from "child_process";
 import { container } from "tsyringe";
 import { Db } from "../src/utils/db";
+import path from "path";
 
-child_process.execSync("flyway -c ./dist/utils/flywayDev migrate", {
-  stdio: "inherit",
-});
+const bin = path.resolve(process.cwd(), "node_modules/.bin");
+
+try {
+  child_process.execSync(`${bin}/flyway -c ./dist/utils/flywayDev migrate`, {
+    stdio: "inherit",
+  });
+} catch (e) {
+  console.error("Flyway migration exception:", e);
+  throw e;
+}
 
 afterAll(async () => {
   const db = container.resolve(Db);
